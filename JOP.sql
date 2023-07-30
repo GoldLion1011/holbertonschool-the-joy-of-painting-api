ee6d2a007666deacf0bd856da5cfe0669e5c8e20
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-07-30 15:13:24 -0500

    initial push and some sql

---
+++ JOP.sql
@@ -0,0 +1,37 @@
+-- Create the "Episodes" table
+CREATE TABLE Episodes (
+  episode_id INT PRIMARY KEY AUTO_INCREMENT,
+  episode_title VARCHAR(255),
+  original_broadcast_date DATE,
+
+);
+
+-- Create the "Subjects" table
+CREATE TABLE Subjects (
+  subject_id INT PRIMARY KEY AUTO_INCREMENT,
+  subject_name VARCHAR(255),
+);
+
+-- Create the "ColorPalettes" table
+CREATE TABLE ColorPalettes (
+  color_id INT PRIMARY KEY AUTO_INCREMENT,
+  color_name VARCHAR(255),
+);
+
+-- Create the "EpisodeSubjects" junction table (Many-to-Many relationship)
+CREATE TABLE EpisodeSubjects (
+  episode_id INT,
+  subject_id INT,
+  PRIMARY KEY (episode_id, subject_id),
+  FOREIGN KEY (episode_id) REFERENCES Episodes(episode_id) ON DELETE CASCADE,
+  FOREIGN KEY (subject_id) REFERENCES Subjects(subject_id) ON DELETE CASCADE
+);
+
+-- Create the "EpisodeColorPalettes" junction table (Many-to-Many relationship)
+CREATE TABLE EpisodeColorPalettes (
+  episode_id INT,
+  color_id INT,
+  PRIMARY KEY (episode_id, color_id),
+  FOREIGN KEY (episode_id) REFERENCES Episodes(episode_id) ON DELETE CASCADE,
+  FOREIGN KEY (color_id) REFERENCES ColorPalettes(color_id) ON DELETE CASCADE
+);

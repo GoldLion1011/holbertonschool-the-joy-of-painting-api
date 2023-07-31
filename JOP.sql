fa1063bdeac82129c7225f33acf12e47c15203be
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-07-31 16:52:38 -0500

    db first draft

--- JOP.sql
+++ JOP.sql
@@ -4,27 +4,28 @@ CREATE DATABASE IF NOT EXISTS JOP;
 USE JOP;
 
 -- Create the "Episodes" table
-CREATE TABLE Episodes (
+CREATE TABLE IF NOT EXISTS Episodes (
   episode_id VARCHAR(6), -- season and episode number
   episode_title VARCHAR(255),
   air_date DATE,
+  subjects_list VARCHAR(255), -- Comma-separated list of subject names associated with the episode
 );
 
 -- Create the "Subjects" table
-CREATE TABLE Subjects (
-  subject_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Subjects (
+  subject_id INT PRIMARY KEY,
   subject_name VARCHAR(255),
 );
 
 -- Create the "ColorPalettes" table
-CREATE TABLE ColorPalettes (
-  color_id INT PRIMARY KEY AUTO_INCREMENT, -- needs to be joined hex_value
+CREATE TABLE IF NOT EXISTS ColorPalettes (
+  color_id VARCHAR(7),
   color_name VARCHAR(255),
-  hex_value VARCHAR(7),
+);
 
 -- Create the "EpisodeSubjects" junction table (Many-to-Many relationship)
-CREATE TABLE EpisodeSubjects (
-  episode_id INT,
+CREATE TABLE IF NOT EXISTS EpisodeSubjects (
+  episode_id VARCHAR(6),
   subject_id INT,
   PRIMARY KEY (episode_id, subject_id),
   FOREIGN KEY (episode_id) REFERENCES Episodes(episode_id) ON DELETE CASCADE,
@@ -32,10 +33,43 @@ CREATE TABLE EpisodeSubjects (
 );
 
 -- Create the "EpisodeColors" junction table (Many-to-Many relationship)
-CREATE TABLE EpisodeColors (
-  episode_id INT,
+CREATE TABLE IF NOT EXISTS EpisodeColors (
+  episode_id VARCHAR(6),
   color_id INT,
   PRIMARY KEY (episode_id, color_id),
   FOREIGN KEY (episode_id) REFERENCES Episodes(episode_id) ON DELETE CASCADE,
   FOREIGN KEY (color_id) REFERENCES ColorPalettes(color_id) ON DELETE CASCADE
 );
+
+LOAD DATA LOCAL INFILE 'The Joy Of Painting - Episode Dates.csv'
+INTO TABLE Episodes
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"';
+
+LOAD DATA LOCAL INFILE 'The Joy Of Painting - Subject Matter.csv'
+INTO TABLE Subjects
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'The Joy Of Painting - Colors Used.csv'
+INTO TABLE ColorPalettes
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+UPDATE Episodes
+SET
+    episode_title = SUBSTRING_INDEX(SUBSTRING_INDEX(csv_row, ',', 1), '"', -1), -- Extract "Winter Moon"
+    air_date = SUBSTRING_INDEX(SUBSTRING_INDEX(csv_row, ',', -1), '"', 1) -- Extract "February 12, 1983"
+    subjects_list = REPLACE(SUBSTRING_INDEX(csv_row, ',', -2), '"', '') -- Extract "subject1, subject2" and remove double quotes
+WHERE
+    csv_row LIKE '"%"';
+    
+UPDATE Subjects SET episode_id = REPLACE(episode, 'S', '');
+
+UPDATE ColorPalettes SET color_id = REPLACE(color_hex, '#', '');
+UPDATE ColorPalettes SET color_name = REPLACE(REPLACE(colors, '\r', ''), '\n', '');
